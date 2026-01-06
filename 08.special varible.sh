d4266d72160a8aa6f03d98dd5f2490c2c5d42fcc
pavan kalyan <pavan667204@gmail.com>
2026-01-06 13:21:48 +0530

    scripts

--- 08.special varible.sh
+++ 08.special varible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
  echo "$@ --------all varible passed the scripted."
- echo "$# ---------number of varib;e passed .(counting)"
+ echo "$# ---------number of varible passed .(counting)"
 echo "$0---------- Scripted name."
 echo "$pwd------- working directory"
 echo "$home----- home directory"
@@ -9,4 +9,4 @@ echo "$$-------- pid scripted execution"
 echo "$!---------last pid for command"
 echo "$?--------- state of previous command  0---> success  1 to 127-->fail"
 echo "set -e automatic exit"
-echo "ser -ex for deburing"
+echo "set -ex for deburing"
