42a77159723224291e79ad5bdba7d9998912d908
pavan kalyan <pavan667204@gmail.com>
2026-01-15 13:37:10 +0530

    scripts

---
+++ healthcheck.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "===== SYSTEM HEALTH CHECK ====="
+date
+echo
+
+echo "Uptime:"
+uptime
+echo
+
+echo "CPU Load:"
+top -b -n1 | head -5
+echo
+
+echo "Memory Usage:"
+free -h
+echo
+
+echo "Disk Usage:"
+df -h
+echo 
+
+echo "ports"
+netstat -lntp
+echo
